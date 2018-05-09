4a6811ce6ba5f283db7003429dfc79a076044dcf
Petter Poucette <petter@tovan.org>
2018-05-09 16:03:05 +0200

    continuing with the structure of the application

--- js_files/controller.js
+++ js_files/controller.js
@@ -1,6 +1,8 @@
 var app = angular.module('app', ['ngRoute']);
 
 app.controller('ctrl', function($scope, $http, $location) {
-  $scope.test="Testing the controller"
+  $scope.test_landning_page="The landning_page works"
+  $scope.test_starship_search="The starship_search page works"
+  $scope.test_create_user="The create_user page works"
 
 });
---
+++ js_files/routning.js
@@ -0,0 +1,17 @@
+app.config(function($routeProvider){
+
+  $routeProvider
+  .when('/', {
+    templateUrl:"views/landing_page.html",
+    controller: "ctrl"
+  })
+  .when('/starship_search', {
+    templateUrl:"views/starship_search.html",
+    controller: "ctrl"
+  })
+  .when('/create_user', {
+    templateUrl:"views/create_user.html",
+    controller: "ctrl"
+  })
+  .otherwise({redirectTo:"/"});
+});
