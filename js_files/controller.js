6c03472346a8687a9eb880b81bbb0fb239287b68
Petter Poucette <petter@tovan.org>
2018-05-10 10:52:28 +0200

    collecting the ships from the api, printning them in a dropdown and presenting the price for each ship

--- js_files/controller.js
+++ js_files/controller.js
@@ -5,4 +5,33 @@ app.controller('ctrl', function($scope, $http, $location) {
   $scope.test_starship_search="The starship_search page works"
   $scope.test_create_user="The create_user page works"
 
+  $scope.starships = getStarships();
+
+  function getStarships(){
+    var pageNr = 1;
+    var starshipArray = [];
+    while(pageNr < 5){
+      $http({
+        method: 'GET',
+        url: 'https://swapi.co/api/starships/?page=' + pageNr,
+      }).then(function(response){
+        angular.forEach(response.data.results,function(value,index){
+          starshipArray.push(value);
+        });
+      });
+      if(pageNr == 4){
+        return starshipArray;
+      }
+      pageNr=pageNr + 1;
+    }
+  }
+
+  $scope.log = function(){
+    console.log("starships inne:" + $scope.starships);
+  }
+
+
+
+
+
 });
