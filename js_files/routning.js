84f44c16f0c89a2a07cfca612585c0d0ec1624c1
Fredrik Östlund <id15fod@cs.umu.se>
2018-05-18 12:03:50 +0200

    Star wars intro added

--- js_files/routning.js
+++ js_files/routning.js
@@ -1,8 +1,12 @@
 app.config(function($routeProvider){
 
   $routeProvider
+  // .when('/', {
+  //   templateUrl:"views/landing_page.html",
+  //   controller: "ctrl"
+  // })
   .when('/', {
-    templateUrl:"views/landing_page.html",
+    templateUrl: "views/intro.html",
     controller: "ctrl"
   })
   .when('/starship_search', {
